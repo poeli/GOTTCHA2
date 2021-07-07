1f385448810352b5598fcdb1ea75bf6a1d286768
Anna Chernikov <annaleezac@gmail.com>
2021-07-07 14:59:22 -0400

    add gtdb_taxonomy

--- gottcha2.py
+++ gottcha2.py
@@ -34,6 +34,7 @@ from re import compile as recompile
 from multiprocessing import Pool
 from itertools import chain
 import math
+import gtdb_taxonomy as gd
 
 def parse_params( ver ):
     p = ap.ArgumentParser( prog='gottcha2.py', description="""Genomic Origin Through Taxonomic CHAllenge (GOTTCHA) is an
@@ -62,7 +63,7 @@ def parse_params( ver ):
 
     p.add_argument( '-np','--nanopore', action="store_true",
                     help="Adjust options for Nanopore reads. The 'mismatch' option will be ignored. [-xm map-ont -mr 1]")
-                    
+
     p.add_argument( '-pm','--mismatch', metavar='<INT>', type=int, default=10,
                     help="Mismatch penalty for the aligner. [default: 10]")
 
@@ -334,7 +335,7 @@ def process_sam_file( sam_fn, numthreads, numlines ):
     return result, mapped_reads
 
 def is_descendant( taxid, taxid_ant ):
-    fullLineage = gt.taxid2fullLineage( taxid )
+    fullLineage = gd.taxid2fullLineage( taxid )
     if "|%s|" % taxid_ant in fullLineage:
         return True
     else:
@@ -369,7 +370,7 @@ def ReadExtractWorker( filename, chunkStart, chunkSize, taxid ):
         if not (pri_aln_flag and valid_flag): continue
 
         acc, start, stop, t = ref.split('|')
-        fullLineage = gt.taxid2fullLineage(t)
+        fullLineage = gd.taxid2fullLineage(t)
 
         if int(flag) & 16:
             g = findall(r'\d+\w', cigr)
@@ -425,7 +426,7 @@ def group_refs_to_strains(r):
     }, inplace=True)
 
     str_df['ZSCORE'] = str_df.apply(lambda x: pile_lvl_zscore(x.TOTAL_BP_MAPPED, x.TOL_SIG_LENGTH, x.LINEAR_LEN), axis=1)
-    
+
     return str_df
 
 def roll_up_taxonomy( r, db_stats, abu_col, tg_rank, mc, mr, ml, mz):
@@ -446,8 +447,8 @@ def roll_up_taxonomy( r, db_stats, abu_col, tg_rank, mc, mr, ml, mz):
                     (str_df['ZSCORE'] <= mz)
 
     for rank in sorted(major_ranks, key=major_ranks.__getitem__):
-        str_df['LVL_NAME'] = str_df['TAXID'].apply(lambda x: gt.taxid2lineageDICT(x, True, True)[rank]['name'])
-        str_df['LVL_TAXID'] = str_df['TAXID'].apply(lambda x: gt.taxid2lineageDICT(x, True, True)[rank]['taxid'])
+        str_df['LVL_NAME'] = str_df['TAXID'].apply(lambda x: gd.taxid2lineageDICT(x, True, True)[rank]['name'])
+        str_df['LVL_TAXID'] = str_df['TAXID'].apply(lambda x: gd.taxid2lineageDICT(x, True, True)[rank]['taxid'])
         str_df['LEVEL'] = rank
 
         # rollup strains that make cutoffs
@@ -459,7 +460,7 @@ def roll_up_taxonomy( r, db_stats, abu_col, tg_rank, mc, mr, ml, mz):
                 'LEVEL':'first',
                 'LVL_TAXID':'first',
                 'TOTAL_BP_MAPPED': sum, 'READ_COUNT': sum, 'TOTAL_BP_MISMATCH': sum,
-                'LINEAR_LEN': sum, 'MAPPED_SIG_LENGTH': sum, 'TOL_SIG_LENGTH': sum, 
+                'LINEAR_LEN': sum, 'MAPPED_SIG_LENGTH': sum, 'TOL_SIG_LENGTH': sum,
                 'ROLLUP_DOC': sum, 'BEST_DOC': max, 'BEST_LINEAR_COV': max, 'ZSCORE': min,
             }).reset_index().copy()
 
@@ -471,11 +472,11 @@ def roll_up_taxonomy( r, db_stats, abu_col, tg_rank, mc, mr, ml, mz):
         lvl_df['NOTE'] = ""
         if major_ranks[rank] > major_ranks[tg_rank]:
             lvl_df['NOTE'] = f"Not shown ({rank}-result biased); "
-        
+
         # concart ranks-dataframe to the report-dataframe
         rep_df = pd.concat([rep_df, lvl_df.sort_values('ABUNDANCE', ascending=False)], sort=False)
 
-    rep_df["LINEAR_COV"] = rep_df["LINEAR_LEN"]/rep_df["TOL_SIG_LENGTH"]      
+    rep_df["LINEAR_COV"] = rep_df["LINEAR_LEN"]/rep_df["TOL_SIG_LENGTH"]
     rep_df["LINEAR_COV_MAPPED_SIG"] = rep_df["LINEAR_LEN"]/rep_df["MAPPED_SIG_LENGTH"]
     rep_df["DOC"] = rep_df["TOTAL_BP_MAPPED"]/rep_df["TOL_SIG_LENGTH"]
     rep_df["LINEAR_DOC"] = rep_df["TOTAL_BP_MAPPED"]/rep_df["LINEAR_LEN"]
@@ -521,9 +522,9 @@ def generaete_taxonomy_file(rep_df, o, fullreport_o, fmt="tsv"):
     sep = ',' if fmt=='csv' else '\t'
     # save full report
     rep_df[cols].to_csv(fullreport_o, index=False, sep=sep, float_format='%.6f')
-    # save summary    
+    # save summary
     qualified_df.to_csv(o, index=False, sep=sep, float_format='%.6f')
-        
+
     return True
 
 def generaete_biom_file(res_df, o, tg_rank, sampleid):
@@ -535,11 +536,11 @@ def generaete_biom_file(res_df, o, tg_rank, sampleid):
     from biom.table import Table
     if biom.__version__ < '2.1.7':
         sys.exit("[ERROR] Biom library requires v2.1.7 or above.\n")
-    
+
     target_df = pd.DataFrame()
     target_idx = (res_df['LEVEL']==tg_rank)
     target_df = res_df.loc[target_idx, ['ABUNDANCE','TAXID']]
-    target_df['LINEAGE'] = target_df['TAXID'].apply(lambda x: gt.taxid2lineage(x, True, True)).str.split('|')
+    target_df['LINEAGE'] = target_df['TAXID'].apply(lambda x: gd.taxid2lineage(x, True, True)).str.split('|')
 
     sample_ids = [sampleid]
     data = np.array(target_df['ABUNDANCE']).reshape(len(target_df), 1)
@@ -554,7 +555,7 @@ def generaete_lineage_file(target_df, o, tg_rank):
     """
     output abundance-lineage in tsv format
     """
-    lineage_df = target_df['TAXID'].apply(lambda x: gt.taxid2lineage(x, True, True)).str.split('|', expand=True)
+    lineage_df = target_df['TAXID'].apply(lambda x: gd.taxid2lineage(x, True, True)).str.split('|', expand=True)
     result = pd.concat([target_df['ABUNDANCE'], lineage_df], axis=1, sort=False)
     result.to_csv(o, index=False, header=False, sep='\t', float_format='%.4f')
 
@@ -565,7 +566,7 @@ def readMapping(reads, db, threads, mm_penalty, presetx, samfile, logfile, nanop
     mapping reads to database
     """
     input_file = " ".join([x.name for x in reads])
-    
+
     sr_opts = f"-x {presetx} --second=no -k24 -A1 -B{mm_penalty} -O30 -E30 -a -N1 -n1 -p1 -m24 -s30"
     if nanopore:
         sr_opts = f"-x {presetx} --second=no -a"
@@ -659,7 +660,7 @@ if __name__ == '__main__':
             outfile = "%s/%s.csv" % (argvs.outdir, argvs.prefix)
         elif argvs.format == "biom":
             outfile = "%s/%s.biom" % (argvs.outdir, argvs.prefix)
-        
+
         out_fp = open(outfile, 'w')
 
     print_message( "Starting GOTTCHA (v%s)" % __version__, argvs.silent, begin_t, logfile )
@@ -683,7 +684,8 @@ if __name__ == '__main__':
     custom_taxa_tsv = None
     if os.path.isfile( argvs.database + ".tax.tsv" ):
         custom_taxa_tsv = argvs.database+".tax.tsv"
-    gt.loadTaxonomy( argvs.taxInfo, custom_taxa_tsv )
+    #gt.loadTaxonomy( argvs.taxInfo, custom_taxa_tsv )
+    gd.loadGTDB('./')
     print_message( "Done.", argvs.silent, begin_t, logfile )
 
     #load database stats
@@ -705,7 +707,7 @@ if __name__ == '__main__':
             print_message( "Done mapping reads to %s signature database." % argvs.dbLevel, argvs.silent, begin_t, logfile )
             print_message( "Mapped SAM file saved to %s." % samfile, argvs.silent, begin_t, logfile )
             sam_fp = open( samfile, "r" )
-    
+
     if argvs.extract:
         extract_read_from_sam( os.path.abspath(samfile), out_fp, argvs.extract, argvs.threads )
         print_message( "Done extracting reads to %s." % outfile, argvs.silent, begin_t, logfile )
---
+++ gtdb_taxonomy.py
@@ -0,0 +1,244 @@
+#!/usr/bin/env python3
+import sys
+import io
+import taxonomy
+import requests
+import wget
+import os
+import tarfile
+
+
+#GLOBAL DICTS
+#key: gtdb reference number, value: ncbi accession
+gtdb_tax = {}
+#key: ncbi accession, gtdb reference number
+ncbi_tax = {}
+#ranks list
+ranks = ['strain','species','genus','family','order','class','phylum','superkingdom']
+#rank dictionary with letters for keys
+rankDict = {'s':'species','g':'genus','f':'family','o':'order','c':'class','p':'phylum','d':'superkingdom'}
+
+#Class for each Taxon in the taxonomy tree
+class Taxon():
+    def __init__(self, assigned_id, gtdb_id, parent):
+         #assignments
+         self.id = assigned_id
+         self.gtdb_id = gtdb_id
+         self.parent = parent
+         splitName = gtdb_id.split("__")
+         #map to ncbi accession if exists
+         try:
+             self.ncbi_id = gtdb_tax[gtdb_id]
+         except:
+             self.ncbi_id = None
+
+        #not a leaf
+         if len(splitName) > 1:
+             self.name = splitName[1]
+             self.rank = rankDict[splitName[0]]
+        #a leaf
+         else:
+             self.name = gtdb_id
+             self.rank = "strain"
+             self.id = gtdb_id
+
+#Taxonomy tree
+class Graph():
+    def __init__(self):
+         self.dictionary = {}
+         self.count = 1
+    def add_node(self, tid, parent):
+        taxon = Taxon(self.count, tid, parent)
+        self.count += 1
+        if tid not in self.dictionary.keys():
+             self.dictionary[tid] = []
+        return taxon
+    def add_edge(self, node1, node2):
+         self.dictionary[node1].append(node2)
+    def get_dictionary(self):
+        return self.dictionary
+    def print_graph(self):
+        self.print_graph_iter('root')
+    def print_graph_iter(self, item):
+        print(item + ": " + str(self.dictionary[item]))
+        items = self.dictionary[item]
+        for i in items:
+             self.print_graph_iter(i)
+
+
+# key: gtdb reference number, value: Node
+nodes = {}
+#graph
+graph = Graph()
+#
+nodes_ncbi = {}
+#
+graph_ncbi = Graph()
+
+#assembly accession to rank
+def taxid2rank(tid):
+    return nodes[tid].rank
+
+#assembly accession to name
+def taxid2name(tid):
+    return nodes[nodes[tid].parent].name
+
+#returns taxonomy in the format of a dictionary
+def taxid2lineage(tid):
+    ret = {}
+    gtdb_id =  ncbi_tax[tid]
+    node = nodes[gtdb_id]
+    parent = node.parent
+    taxid = node.id
+    i = 0
+    ret[ranks[i]] = {}
+    ret[ranks[i]]['name'] = taxid
+    ret[ranks[i]]['taxid'] = taxid
+    while parent != 'root':
+        i += 1
+        node = nodes[parent]
+        parent = node.parent
+        name = node.name
+        taxid = node.id
+        ret[ranks[i]] = {}
+        ret[ranks[i]]['name'] = name
+        ret[ranks[i]]['taxid'] = taxid
+    return ret
+
+def taxid2fullLineage( taxID ):
+	n = nodes[taxID]
+	fullLineage = ""
+
+	while taxID != '1':
+		rank = taxid2ranks(taxID)
+		name = taxid2name(taxID)
+		if not name: break
+		fullLineage += "%s|%s|%s|"%(rank,taxID,name)
+		taxID = n.parent
+
+	return fullLineage
+
+#loading wol metadata format (unused method)
+def loadWOL(path):
+    with open(path) as f:
+        header = f.readline().rstrip('\r\n').split('\t')
+        ncbi_id = header.index('assembly_accession')
+        gtdb_id = header.index('gtdb_id')
+        for line in f:
+            line = line.rstrip('\r\n').split('\t')
+            ncbi_tax[line[ncbi_id]] = line[gtdb_id]
+
+#loading gtdb metadata and taxonomy from a directory path
+def loadGTDB(path):
+    if path is None:
+        url = 'https://data.ace.uq.edu.au/public/gtdb/data/releases/latest/'
+        bac_taxonomy = url + 'bac120_taxonomy.tsv'
+        bac_metadata = url + 'bac120_metadata.tar.gz'
+        arch_taxonomy = url + 'ar122_taxonomy.tsv'
+        arch_metadata = url + 'ar122_metadata.tar.gz'
+        sys.stderr.write( f"[INFO] Auto downloading taxonomy from {url}...\n" )
+        # download taxonomy file if auto_download enabled
+        r = requests.get(bac_taxonomy, stream=True)
+        open('bac120_taxonomy.tsv', 'wb').write(r.content)
+        r=requests.get(bac_metadata, stream=True)
+        open('bac120_metadata.tar.gz', 'wb').write(r.content)
+        r=requests.get(arch_taxonomy, stream=True)
+        open('ar122_taxonomy.tsv', 'wb').write(r.content)
+        r=requests.get(arch_metadata, stream=True)
+        open('ar122_metadata.tar.gz', 'wb').write(r.content)
+        #extract metadatafiles
+        tar = tarfile.open('bac120_metadata.tar.gz','r:gz')
+        tar.extractall()
+        tar.close()
+        os.rename('bac120_metadata_r202.tsv','bac120_metadata.tsv')
+        tar = tarfile.open('ar122_metadata.tar.gz','r:gz')
+        tar.extractall()
+        tar.close()
+        os.rename('ar122_metadata_r202.tsv','ar122_metadata.tsv')
+        path=""
+
+
+    loadGTDBMetadata(str(path) + 'bac120_metadata.tsv')
+    loadGTDBMetadata(str(path) + 'ar122_metadata.tsv')
+    loadGTDBtaxonomy(str(path) + 'bac120_taxonomy.tsv')
+    loadGTDBtaxonomy(str(path) + 'ar122_taxonomy.tsv')
+
+#Load NCBI Taxonomies from GTDB Metadata file
+def loadNCBI(path):
+    if path is None:
+        url = 'https://data.ace.uq.edu.au/public/gtdb/data/releases/latest/'
+        bac_metadata = url + 'bac120_metadata.tar.gz'
+        arch_metadata = url + 'ar122_metadata.tar.gz'
+        sys.stderr.write( f"[INFO] Auto downloading taxanomy from {url}...\n" )
+        # download taxonomy file if auto_download enabled
+        wget.download(bac_metadata)
+        wget.download(arch_metadata)
+    loadNCBITaxonomy(str(path) + 'bac120_metadata.tsv')
+    loadNCBITaxonomy(str(path) + 'ar122_metadata.tsv')
+
+def loadNCBITaxonomy(metadata):
+    if not os.path.isfile(metadata):
+        raise Exception(metadata + "File Not Found")
+    graph_ncbi.add_node('root', None)
+    with open(metadata, encoding="utf-8") as f:
+        header = f.readline().rstrip('\r\n').split('\t')
+        gtdb_id = header.index("gtdb_genome_representative")
+        ncbi_tax = header.index("ncbi_taxonomy")
+        for line in f:
+            line = line.rstrip('\r\n').split('\t')
+            ncbi_id = '_'.join(line[gtdb_id].split("_")[1:])
+            tax = line[ncbi_tax].split(";")
+            tax.append(line[gtdb_id])
+            parent = 'root'
+            for i,c in enumerate(tax):
+                taxon = graph_ncbi.add_node(c,parent)
+                parent = c
+                nodes_ncbi[c] = taxon
+                if i == 0:
+                    graph_ncbi.add_edge('root',c)
+                else:
+                    graph_ncbi.add_edge(tax[i-1],c)
+                if not line:
+                    continue
+
+
+#load metadata file
+def loadGTDBMetadata(metadata):
+    if not os.path.isfile(metadata):
+        raise Exception(metadata + " File Not Found")
+    with open(metadata, encoding="utf-8") as f:
+        header = f.readline().rstrip('\r\n').split('\t')
+        gtdb_id = header.index("gtdb_genome_representative")
+        for line in f:
+            line = line.rstrip('\r\n').split('\t')
+            ncbi_id = '_'.join(line[gtdb_id].split("_")[1:])
+            gtdb_tax[line[gtdb_id]] = ncbi_id
+            ncbi_tax[ncbi_id] = line[gtdb_id]
+
+#load taxonomy file
+def loadGTDBtaxonomy(taxonomy):
+    if not os.path.isfile(taxonomy):
+        raise Exception(taxonomy + "File Not Found")
+    graph.add_node('root', None)
+    with open(taxonomy, encoding="utf-8") as f:
+        for line in f:
+            line = line.rstrip('\r\n')
+            gtdb_id, taxa = line.split('\t',1)
+            tax = taxa.split(';')
+            tax.append(gtdb_id)
+            parent = 'root'
+            for i,c in enumerate(tax):
+                taxon = graph.add_node(c, parent)
+                parent = c
+                nodes[c] = taxon
+                if i == 0:
+                     graph.add_edge('root', c)
+                else:
+                     graph.add_edge(tax[i-1], c)
+            if not line:
+                continue
+
+
+if __name__ == '__main__':
+    loadGTDB(sys.argv[1] if len(sys.argv)>1 else None)
+    loadNCBI(sys.argv[1] if len(sys.argv)>1 else None)
