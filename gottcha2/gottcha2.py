94cbd64e1d3f1615aa4eaac624d769143d316cd8
Anna Chernikov <annaleezac@gmail.com>
2021-08-25 17:26:01 -0700

    add logfile

--- gottcha2/gottcha2.py
+++ gottcha2/gottcha2.py
@@ -623,8 +623,9 @@ def print_message(msg, silent, start, logfile, errorout=0):
 
 def main(args):
     global argvs
-    argvs = parse_params( __version__, args )
+    global logfile
     global begin_t
+    argvs = parse_params( __version__, args )    
     begin_t  = time.time()
     sam_fp   = argvs.sam[0] if argvs.sam else ""
     samfile  = "%s/%s.gottcha_%s.sam" % ( argvs.outdir, argvs.prefix, argvs.dbLevel ) if not argvs.sam else sam_fp.name
